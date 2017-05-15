d6b7bf87c07f5c7eaa49bb08ae171cab802ae205
Jurica Separovic <jsepar00@fesb.hr>
2017-05-15 21:31:22 +0000

    forntend setup

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         Pitaj\Providers\EventServiceProvider::class,
         Pitaj\Providers\RouteServiceProvider::class,
 
+        /**
+         * Materialize Sass
+         */
+        Qntm\LaravelMaterializeSass\MaterializeSassServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /**
+         * Aliasses for custom classes
+         */
+        'MaterializeSass' => Qntm\LaravelMaterializeSass\MaterializeSassBuilder::class,
 
     ],
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,32 +8,46 @@
         <title>Pitaj | Imamo odgovor za sve </title>
 
         <!-- Styles -->
+        <link rel="stylesheet" href="/css/materialize.css">
         <link rel="stylesheet" href="/css/app.css">
+
+        <!-- Materialize js -->
+        {!! MaterializeSass::includeMaterialize() !!}
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <blockquote>
+                    This is an example quotation that uses the blockquote tag.
+                </blockquote>
+                <h1> Materialize header </h1>
+                <div class="divider"></div>
+                <div class="section">
+                    <h5>Section 1</h5>
+                    <p>Stuff</p>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="row collection">
+                    <a href="#" class="collection-item"><span class="badge">1</span>Alan</a>
+                    <a href="#" class="collection-item"><span class="new badge">4</span>Alan</a>
+                    <a href="#" class="collection-item">Alan</a>
+                    <a href="#" class="collection-item"><span class="badge">14</span>Alan</a>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="divider"></div>
+                <div class="row col s12 z-depth-1">
+                    <div class="section">
+                        <h5>Section 2</h5>
+                        <p>Stuff</p>
+                    </div>
                 </div>
             </div>
         </div>
