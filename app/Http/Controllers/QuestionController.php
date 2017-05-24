5dc3ad084cd5aae4ba918e954c564c175f45b16f
Jurica Separovic <jsepar00@fesb.hr>
2017-05-24 21:37:22 +0000

    question changes

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pitaj\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    public function showForm()
+    {
+        //show ask question
+    }
+}
