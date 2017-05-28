8968c387e3edc1a528ed7c001aea6d77729a0acc
Jurica Separovic <jsepar00@fesb.hr>
2017-05-28 15:35:38 +0000

    some styleing on answers and single question

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -3,43 +3,51 @@
 @section('title', $question->title)
 
 @section('content')
-    <div class="col s12 m4 l8">
-        <h5>Aktualno</h5>
-        <hr>
-        <div class="card-panel z-depth-1 lighten-5">
-            <blockquote>
-                <a href="{{ route('question.single', ['id' => $question->id , 'slug' => $question->slug]) }}">
-                    {{ $question->title }}
-                </a>
-            </blockquote>
-            <div class="row" id="tags">
-                <div class="col">
-                    @foreach( $question->tags as $tag )
-                        <span class="new badge">
-                    {{ $tag->name }}
-                </span>
-                    @endforeach
-                </div>
-            </div>
+    <div class="card-panel col s12 m8 l8">
+        <header class="section" id="questionTitle">
+            <h5 class="h1"> {{ $question->title }}</h5>
             <i>
-                <i class="tiny material-icons">person_pin</i>
-                {{ $question->author()->first()->name }} on
+                {{ $question->author->name }}
+                {{ $question->author->lastName }}
                 {{ $question->createdForHuman() }}
             </i>
-        </div>
+            <div class="divider"></div>
+        </header>
+        <main id="questionContent">
+            <section>
+                <p>Question body text here</p>
+            </section>
+            <section class="section" id="tags">
+                @foreach( $question->tags as $tag )
+                    <div class="chip">
+                        {{ $tag->name }}
+                    </div>
+                @endforeach
+            </section>
+        </main>
     </div>
+    {{-- Answers area --}}
+    <div class="col s12 m8 l8">
+        @if(count($answers = $question->answers()->get()) > 0)
+            <h6>Komentara <span style="color:red; font-weight: 600">8</span></h6>
+            {{--Display all answers--}}
+            @each('question.answers' , $answers, 'answer')
+        @else
+            <h6>No comments yet</h6>
+        @endif
+    </div>
+
 @endsection
 
 @section('sidebar')
     <div class="col s12 m4 l4">
         <h5>Povezano</h5>
-        <hr>
-    </div>
-    <div class="col s12 m4 l4">
-        <h5>Stats</h5>
-        <div id="row">
-            <div class="col">
-                <span> <i class="tiny material-icons">pageview</i> <span>{{ $question->views }}</span></span>
+        <div class="section">
+            <h5>Stats</h5>
+            <div id="row">
+                <div class="col">
+                    <span> <i class="tiny material-icons">pageview</i> <span>{{ $question->views }}</span></span>
+                </div>
             </div>
         </div>
     </div>
