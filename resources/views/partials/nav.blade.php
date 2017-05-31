2782d573e52e1c157173a5ecf570b281de10e5f0
Jurica Šeparović <jsepar00@fesb.hr>
2017-05-31 20:59:32 +0200

    10 tags popular

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,19 +1,21 @@
-<nav>
-    <div class="navbar-fixed">
-        <div class="row">
-            <div class="col s12">
-                <a href="{{ route('home') }}" class="brand-logo">Pitaj</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
+<div class="navbar-fixed">
+    <nav>
+        <div class="navbar-fixed">
+            <div class="row">
+                <div class="col s12">
+                    <a href="{{ route('home') }}" class="brand-logo">Pitaj</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <li><a href="{{ route('question.askForm') }}">Postavi pitanje</a></li>
-                    @if( Auth::check() )
-                        <li><a href="">Profil</a></li>
-                        <li><a href="{{ route('session.logOut') }}">Odjava</a></li>
-                    @else
-                        <li><a href="{{ route('session.loginForm') }}">Prijava</a></li>
-                        <li><a href="{{ route('register.showForm') }}">Registracija</a></li>
-                    @endif
-                </ul>
+                        @if( Auth::check() )
+                            <li><a href="">Profil</a></li>
+                            <li><a href="{{ route('session.logOut') }}">Odjava</a></li>
+                        @else
+                            <li><a href="{{ route('session.loginForm') }}">Prijava</a></li>
+                            <li><a href="{{ route('register.showForm') }}">Registracija</a></li>
+                        @endif
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
