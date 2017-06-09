304876ac4252b9f750fafb9e123493d03dcf8ce4
Jurica Šeparović <jsepar00@fesb.hr>
2017-06-09 22:03:44 +0200

    tag fix

--- resources/views/question/add.blade.php
+++ resources/views/question/add.blade.php
@@ -17,56 +17,25 @@
             <div class="input-field col s12">
                 <textarea type="textarea" id="questionBody" name="questionBody"
                           class="materialize-textarea"
-                          data-length="600"
+                          data-length="2000"
                           placeholder="Detaljnije opišite vaše pitanje ukoliko je to potrebno"></textarea>
                 <label for="questionBody" data-success="Dobro pitanje">Dodatno objašnjenje</label>
             </div>
         </div>
         <div class="row">
-            <div class="input-field">
-                <div class="col s12">
-                    <label>Tagirajte pitanje</label>
-                    <div class="chips chips-placeholder"></div>
-                </div>
+            <div class="col s12">
+                <label id="chip" class="validate">Tagirajte pitanje</label>
+                <div class="chips chips-placeholder"></div>
             </div>
         </div>
-        <input id="tags" type="hidden" value="" name="tags">
         <div class="row">
             @include('partials.errors')
         </div>
-        <button id="questionForm" type="button" onclick="onQuestionSubmit()" class="btn waves-effect waves-light col s6 offset-s3">
-            Pitaj
-        </button>
+        <div class="row">
+            <button id="questionFormSubmit" type="button" class="btn waves-effect waves-light col s6 offset-s3">
+                Pitaj
+            </button>
+        </div>
     </form>
-    <script>
-        function onQuestionSubmit() {
-            let token = $('#token').val();
-            console.log(token);
-            let titleDiv = $('#questionTitle');
-
-            let title = titleDiv.val();
-            let body = $('#questionBody').val();
-
-            console.log(title,body);
-
-            chips = $('.chips').material_chip('data');
-
-            if (title === '' || chips.length < 1) {
-                $('label[for="questionBody"]').attr('data-error', 'Polje je obavezno');
-                titleDiv.removeClass("valid");
-                titleDiv.addClass("invalid");
-            } else {
-                chips = JSON.stringify(chips);
-                $.post('/pitaj', {
-                    title : title,
-                    body : body,
-                    tags : chips,
-                    _token : token
-                }).done(function (questionId) {
-                    window.location.href = "/pitanja/" + questionId;
-                });
-            }
-        }
-    </script>
 @overwrite
 
