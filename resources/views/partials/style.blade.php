133ead41d19a214df7445cacd5cb8944f91e8e48
Jurica Šeparović <jsepar00@fesb.hr>
2017-07-04 19:50:01 +0200

    some styles

--- resources/views/partials/style.blade.php
+++ resources/views/partials/style.blade.php
@@ -1,4 +1,2 @@
-<!-- Styles -->
 <!-- Compiled and minified CSS -->
-{{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">--}}
 <link rel="stylesheet" href="{{ asset('css/app.css') }} ">
