2516b630828f4074faff1c9aa31f9ea7e83da9da
Jurica Šeparović <jsepar00@fesb.hr>
2017-07-05 21:51:04 +0200

    Seed files that i don't use

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,37 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Pitaj\User::class, function (Faker\Generator $faker) {
+$factory->define(Pitaj\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'avatar' => $faker->imageUrl('48','48'),
+        'password' => $password ?: $password = bcrypt('sifra'),
+        'activated' => 1,
         'remember_token' => str_random(10),
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Pitaj\Models\Question::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->name,
+        'slug' => $faker->unique()->safeEmail,
+        'body' => $faker->imageUrl('48','48'),
+        'author_id' => function () {
+            return factory(Pitaj\Models\User::class)->create();
+        },
+        'views' => $faker->numberBetween(342, 999999),
+        'remember_token' => str_random(10),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Pitaj\Models\Tag::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
+
