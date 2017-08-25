d3d99ddc72f3ff0e083f0d570439d11a297a2dc6
Jurica Šeparović <jsepar00@fesb.hr>
2017-08-25 22:31:58 +0200

    fixed answers hasVoted on null

--- resources/views/answer/answers.blade.php
+++ resources/views/answer/answers.blade.php
@@ -6,26 +6,44 @@
                     <p>{{ $answer->body }}</p>
                 </section>
             </div>
-        </div>
+        @if(Auth::user())
+            <div class="col s2 center-align">
+                <div class="section">
+                    <div id="upVote">
+                        <a href="{{ route('answer.vote', ['id' => $answer->id, 'action' => 'up'] )}}"
+                           class="material-icons {{ Auth::user()->hasVoted($answer, "up") ? "blue-text" : "grey-text" }}">thumb_up</a>
+                    </div>
+                    <div id="votes">
+                        <span class="votesNumber">{{ $answer->votes->sum('vote_value') }}</span>
+                    </div>
+                    <div id="downVote">
+                        <a href="{{ route('answer.vote', ['id' => $answer->id, 'action' => 'down'] )}}"
+                           class="material-icons {{ Auth::user()->hasVoted($answer, "down") ? "blue-text" : "grey-text" }}">thumb_down</a>
+                    </div>
+                </div>
+            </div>
+        @else
         <div class="col s2 center-align">
             <div class="section">
                 <div id="upVote">
                     <a href="{{ route('answer.vote', ['id' => $answer->id, 'action' => 'up'] )}}"
-                       class="material-icons {{ Auth::user()->hasVoted($answer, "up") ? "blue-text" : "grey-text" }}">thumb_up</a>
+                       class="material-icons grey-text">thumb_up</a>
                 </div>
                 <div id="votes">
                     <span class="votesNumber">{{ $answer->votes->sum('vote_value') }}</span>
                 </div>
                 <div id="downVote">
                     <a href="{{ route('answer.vote', ['id' => $answer->id, 'action' => 'down'] )}}"
-                       class="material-icons {{ Auth::user()->hasVoted($answer, "down") ? "blue-text" : "grey-text" }}">thumb_down</a>
+                       class="material-icons grey-text">thumb_down</a>
                 </div>
             </div>
+        @endif
         </div>
-        <div class="col s12">
+         <div class="col s12">
             <div id="questionTitle">
                 <i><a href="{{ $answer->author->profileUrl() }}">{{ $answer->author->name }} {{ $answer->author->lastName }}</a></i>
             </div>
         </div>
-    </div>
+     </div>
+</div>
 </div>
